8af65ff8a3e2fa1888fbcd42bb647a784cd2bc6d
Zak <zak@dazestudios.com>
2026-08-01 13:08:26 -0400

    Make analytics compatible with CSP

---
+++ analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+window.gtag = function gtag() {
+  window.dataLayer.push(arguments);
+};
+
+window.gtag("js", new Date());
+window.gtag("config", "G-0DF8FQVZE2");
